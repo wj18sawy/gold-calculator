9a8b296d20b66c07c3e6aaeb6a75de6c0b013581
Wyatt Sawyer <wj18sawy@alum.siena.edu>
2018-12-06 00:40:54 -0500

    Updates

---
+++ precache-manifest.942c4f5db57b54d82b62c0de67cabe34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a77022a14f61e5e3022",
+    "url": "/gold-calculator/static/css/main.bfab37f0.chunk.css"
+  },
+  {
+    "revision": "5a77022a14f61e5e3022",
+    "url": "/gold-calculator/static/js/main.5a77022a.chunk.js"
+  },
+  {
+    "revision": "e80f4e95a969da5dd424",
+    "url": "/gold-calculator/static/css/1.d96a8e80.chunk.css"
+  },
+  {
+    "revision": "e80f4e95a969da5dd424",
+    "url": "/gold-calculator/static/js/1.e80f4e95.chunk.js"
+  },
+  {
+    "revision": "5bdb8a904d28cab99a8c",
+    "url": "/gold-calculator/static/js/runtime~main.5bdb8a90.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/gold-calculator/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/gold-calculator/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/gold-calculator/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/gold-calculator/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/gold-calculator/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "ff3db53990213c38d84dc50b6266d13e",
+    "url": "/gold-calculator/index.html"
+  }
+];
