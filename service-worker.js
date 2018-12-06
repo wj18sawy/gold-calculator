11dd350dc3e6ecb88e91c6d81ab5c76a007b8c2e
Wyatt Sawyer <wj18sawy@alum.siena.edu>
2018-12-06 11:59:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gold-calculator/precache-manifest.44b48702619d71b4574ed715140a601b.js"
+  "/gold-calculator/precache-manifest.6750a58732ae2a6939f9960ea36fcba7.js"
 );
 
 workbox.clientsClaim();
